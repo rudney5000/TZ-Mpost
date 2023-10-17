c0e2ab1bb846eb0ac0bf0bd7940a8f06a6c87dd8
bonheurbaleks <bonheurbalekoua@gmail.com>
2023-10-18 01:40:44 +0300

    added components

---
+++ components/predictionmarket/CreateMarketParent.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import YesNoButton from "./YesNoButton";
+import CreateMarketChild from "./CreateMarketChild";
+
+export default function CreateMarket() {
+  const [market, setMarket] = useState("");
+
+  return (
+    <>
+      <div class="flex flex-col items-center pr-20 pl-20 mt-10">
+        <input
+          value={market}
+          onChange={(e) => setMarket(e.target.value)}
+          placeholder="Set market"
+        />
+        <CreateMarketChild input={market} />
+
+        <YesNoButton />
+      </div>
+    </>
+  );
+}
