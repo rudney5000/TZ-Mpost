2a73ad65c6f43fe2642f1a0d4994ef24b31a3fa5
Your Name <you@example.com>
2023-10-18 20:24:08 +0300

    LoginPage

--- components/mainpagss/authPages/login/LoginPage.js
+++ components/mainpagss/authPages/login/LoginPage.js
@@ -1,24 +1,90 @@
 import React from "react";
 import {Input} from "@/components/layout/Input";
+import {
+    Button,
+    Card,
+    Checkbox,
+    Typography
+} from "@material-tailwind/react";
+import {Link} from "react-router-dom";
 
 
 export default function login() {
     return (
-     <div className="">
-         <div className="h-screen w-screen flex justify-center items-center">
-             <div className="w-full md:w-1 mx-2 bg-gray-400 p-2 rounded-lg">
-                 <h1 className="text-2xl font-bold">Login</h1>
-                 <p>Welcome Back</p>
-             </div>
-
-             <div className="mt-5">
-                 <label htmlFor="email">
-                     Email
-                 </label>
-                 <Input type="email" placeholder="Enter you email" id="email"/>
-             </div>
-         </div>
-
-     </div>
-    )
+        <div className="flex items-center justify-center">
+        <Card color="transparent" className="m-6 text-gray-900 dark:text-white flex justify-center rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-blue-950">
+            <Typography variant="h4" color="blue-gray">
+                Sign Up
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal">
+                Nice to meet you! Enter your details to register.
+            </Typography>
+            <form className="mt-8 mb-6 w-80 max-w-screen-lg sm:w-96">
+                <div className="mb-1 flex flex-col gap-6">
+                    <Typography variant="h6" color="blue-gray" className="-mb-3 font-medium text-gray-900 dark:text-white">
+                        Your Name
+                    </Typography>
+                    <Input
+                        size="lg"
+                        placeholder="name@mail.com"
+                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                        labelProps={{
+                            className: "before:content-none after:content-none",
+                        }}
+                    />
+                    <Typography variant="h6" color="blue-gray" className="-mb-3">
+                        Your Email
+                    </Typography>
+                    <Input
+                        size="lg"
+                        placeholder="name@mail.com"
+                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                        labelProps={{
+                            className: "before:content-none after:content-none",
+                        }}
+                    />
+                    <Typography variant="h6" color="blue-gray" className="-mb-3">
+                        Password
+                    </Typography>
+                    <Input
+                        type="password"
+                        size="lg"
+                        placeholder="********"
+                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                        labelProps={{
+                            className: "before:content-none after:content-none",
+                        }}
+                    />
+                </div>
+                <Checkbox
+                    label={
+                        <Typography
+                            variant="small"
+                            color="gray"
+                            className="flex items-center font-normal"
+                        >
+                            I agree the
+                            <a
+                                href="#"
+                                className="font-medium transition-colors hover:text-gray-900"
+                            >
+                                &nbsp;Terms and Conditions
+                            </a>
+                        </Typography>
+                    }
+                    containerProps={{ className: "-ml-2.5" }}
+                />
+                <Button className="mt-6" fullWidth>
+                    sign up
+                </Button>
+                <Typography color="gray" className="mt-4 text-center font-normal">
+                    Already have an account?{" "}
+                    <Link to="/register" className="font-medium text-gray-900 ">
+                        Sign In
+                    </Link>
+                </Typography>
+            </form>
+        </Card>
+        </div>
+    );
 }
